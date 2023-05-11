6905001858c89e18e86ae95ebba4e1e2e6916d7c
KaisarSati <kaisar.aron20016@gmail.com>
2023-05-11 12:48:10 +0500

    Числовые утилиты

--- abc-js/practice/utils/number-utils/floor.js
+++ abc-js/practice/utils/number-utils/floor.js
@@ -1,3 +1,6 @@
 /** возвращает целую часть числа уменьшенную в меньшую сторону */
 export function floor(value) {
+    if (typeof value !== 'number') throw Error ('value must be only number type');
+    var intPart = value | 0;
+    return value >= 0 || value === intPart ? intPart : intPart - 1;
 }
